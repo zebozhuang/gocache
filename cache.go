574d968070a674ea7c75c183cf951f51361586e4
zhuangzebo <zhuangzebo0@gmail.com>
2015-04-11 22:18:07 +0800

    add incr and incrby

--- cache.go
+++ cache.go
@@ -1,14 +1,14 @@
 package cache
 
 import (
-	//	"fmt"
+	"errors"
+	"fmt"
 	"sync"
 	"time"
 )
 
 const (
-	NoExpiration      time.Duration = -1
-	DefaultExpiration time.Duration = 0
+	NoExpiration time.Duration = -1
 )
 
 type Item struct {
@@ -25,8 +25,7 @@ func (item *Item) Expired() bool {
 
 type cache struct {
 	sync.RWMutex
-	items             map[string]*Item
-	defaultExpiration time.Duration
+	items map[string]*Item
 }
 
 func (c *cache) Set(k string, v interface{}, d time.Duration) {
@@ -37,31 +36,69 @@ func (c *cache) Set(k string, v interface{}, d time.Duration) {
 
 func (c *cache) set(k string, v interface{}, d time.Duration) {
 	var e time.Time
-	if d == DefaultExpiration {
-		d = c.defaultExpiration
-	}
-	if d > 0 {
+	var item Item
+
+	item.Value = v
+
+	if d >= 0 {
 		e = time.Now().Add(d)
+		item.Expiration = &e
 	}
 
-	c.items[k] = &Item{
-		Value:      v,
-		Expiration: &e,
+	c.items[k] = &item
+}
+
+func (c *cache) IncrBy(key string, delta int64) (int64, error) {
+	c.Lock()
+	item, ok := c.items[key]
+	if !ok || item.Expired() { // key does not exist or expired.
+		c.set(key, delta, NoExpiration)
+		c.Unlock()
+		return delta, nil
 	}
+
+	value, ok := item.Value.(int64)
+	if !ok {
+		return 0, errors.New(fmt.Sprintf("value type is not integer."))
+	}
+
+	newValue := value + delta
+	item.Value = newValue
+	c.Unlock()
+	return newValue, nil
 }
 
-func (c *cache) IncrBy(key string, delta int64) error {
-	return nil
-}
-
-func (c *cache) Incr(key string) error {
+func (c *cache) Incr(key string) (int64, error) {
 	return c.IncrBy(key, 1)
 }
 
-func NewCache(defaultExpiration time.Duration) *cache {
+func (c *cache) Get(key string) (interface{}, error) {
+	c.RLock()
+	v, err := c.get(key)
+	c.RUnlock()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return v.Value, nil
+}
+
+func (c *cache) get(key string) (*Item, error) {
+	v, ok := c.items[key]
+	if !ok {
+		return nil, errors.New(fmt.Sprintf("key %s does not exist.", key))
+	}
+
+	if v.Expired() {
+		return nil, errors.New(fmt.Sprintf("key %s has expired.", key))
+	}
+	return v, nil
+}
+
+func NewCache() *cache {
 	c := new(cache)
 	c.items = map[string]*Item{}
-	c.defaultExpiration = DefaultExpiration
 
 	return c
 }
--- cache_test.go
+++ cache_test.go
@@ -1,14 +1,78 @@
 package cache
 
 import (
+	"fmt"
 	"testing"
+	"time"
 )
 
 func Test_Time(t *testing.T) {
 
 }
 
-func Test_NewCache(t *testing.T) {
-    c := NewCache(DefaultExpiration)
-    c.Set("k123", "v123", DefaultExpiration)  
+func Test_Set(t *testing.T) {
+	c := NewCache()
+
+	ti := time.Now()
+	k := fmt.Sprintf("%d", ti.Nanosecond())
+	v := ti.Nanosecond()
+
+	c.Set(k, v, NoExpiration)
+}
+
+func Test_Get(t *testing.T) {
+	c := NewCache()
+
+	ti := time.Now()
+	k := fmt.Sprintf("k_%d", ti.Nanosecond())
+	v := fmt.Sprintf("v_%d", ti.Nanosecond())
+
+	c.Set(k, v, NoExpiration)
+
+	vv, err := c.Get(k)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if _v, ok := vv.(string); !ok {
+		t.Fatal("Fail to get Value: %s", _v)
+	}
+}
+
+func Test_Incr(t *testing.T) {
+	c := NewCache()
+
+	k := "q2"
+
+	v, err := c.Incr(k)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if v != 1 {
+		t.Fatal("incr error: %d != 1", v)
+	}
+}
+
+func Test_IncrBy(t *testing.T) {
+	c := NewCache()
+	k := "q3"
+
+	v, err := c.IncrBy(k, 10)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if v != 10 {
+		t.Fatal("incrby err: %d = 10", v)
+	}
+
+	v, err = c.IncrBy(k, 20)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if v != 30 {
+		t.Fatal("incrby err: %d != 30", v)
+	}
 }
